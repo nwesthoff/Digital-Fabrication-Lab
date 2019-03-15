82cf2fb6bfd84e2114e1d899ce71ef83c5706d18
Avelien <avelienhusen@hotmail.com>
2019-03-15 14:41:00 +0100

    :tada: Now you can upload your project file to Firbase

--- client/app/src/main/java/com/nilswesthoff/nils/digitalfabricationlab/Order.java
+++ client/app/src/main/java/com/nilswesthoff/nils/digitalfabricationlab/Order.java
@@ -1,18 +1,34 @@
 package com.nilswesthoff.nils.digitalfabricationlab;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 
 public class Order extends AppCompatActivity implements View.OnClickListener {
 
     private static final int PICK_FILE_REQUEST = 234;
     private Button chooseButton, uploadButton;
+
+    private Uri filePath;
+
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +42,11 @@ public class Order extends AppCompatActivity implements View.OnClickListener {
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
 
+        storageReference = FirebaseStorage.getInstance().getReference();
+
         chooseButton = (Button) findViewById(R.id.choose_button);
         uploadButton = (Button) findViewById(R.id.upload_button);
+
         chooseButton.setOnClickListener(this);
         uploadButton.setOnClickListener(this);
 
@@ -41,6 +60,53 @@ public class Order extends AppCompatActivity implements View.OnClickListener {
 
     }
 
+    private void uploadFile() {
+
+        if (filePath != null) {
+
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+
+            StorageReference riversRef = storageReference.child("images/projects.jpg");
+
+            riversRef.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "File Uploaded", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                            progressDialog.setMessage(((int) progress) + "% Uploaded...");
+                        }
+                    });
+            ;
+        }else{
+            //display an error toast
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == PICK_FILE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            filePath = data.getData();
+        }
+    }
+
     @Override
     public void onClick (View view) {
         if (view == chooseButton) {
@@ -48,6 +114,7 @@ public class Order extends AppCompatActivity implements View.OnClickListener {
             showFileChooser();
         } else if (view == uploadButton){
             //upload to firebase storage
+            uploadFile();
         }
 
     }
