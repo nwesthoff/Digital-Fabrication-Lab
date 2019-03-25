00898fb4e034f394df9ff70fc2304acdf99baa61
Avelien <avelienhusen@hotmail.com>
2019-03-25 14:54:01 +0100

    25 maart

--- client/app/src/main/java/com/nilswesthoff/nils/digitalfabricationlab/Tab1Fragment.java
+++ client/app/src/main/java/com/nilswesthoff/nils/digitalfabricationlab/Tab1Fragment.java
@@ -10,12 +10,12 @@ import android.view.ViewGroup;
 public class Tab1Fragment extends Fragment {
     private static final String TAG = "Tab1Fragment";
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater, inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_requests,container,false);
+   // @Nullable
+   // @Override
+   // public View onCreateView(LayoutInflater, inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+   //     View view = inflater.inflate(R.layout.fragment_requests,container,false);
 
 
-        return view;
-    }
+    //    return view;
+   // }
 }
