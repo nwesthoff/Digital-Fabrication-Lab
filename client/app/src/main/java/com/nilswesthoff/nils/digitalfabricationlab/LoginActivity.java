89e487c6e2d777dd24116cebb49198bf95ee223f
Nils Westhoff <nwesthoff@conceptivate.nl>
2019-03-27 12:23:09 +0100

    🐛fix merge bugs

--- client/app/src/main/java/com/nilswesthoff/nils/digitalfabricationlab/LoginActivity.java
+++ client/app/src/main/java/com/nilswesthoff/nils/digitalfabricationlab/LoginActivity.java
@@ -1,20 +1,10 @@
 package com.nilswesthoff.nils.digitalfabricationlab;
 
-<<<<<<< HEAD
-=======
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.app.DownloadManager;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.CursorLoader;
->>>>>>> 9b5dee77fa266c2453cc82de69b25595beeefd62
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -126,56 +116,5 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 });
     }
-
-<<<<<<< HEAD
-=======
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return false;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-                Intent myIntent=new Intent(LoginActivity.this,Requests.class);
-                LoginActivity.this.startActivity(myIntent);
->>>>>>> 9b5dee77fa266c2453cc82de69b25595beeefd62
-
 }
 
