8311a41cb3269af9e0a40e86f0a8da92dd585ff7
xxannemar <48351336+xxannemar@users.noreply.github.com>
2019-04-03 17:28:49 +0200

    :heart: add likes

--- client/app/src/main/java/com/nilswesthoff/nils/digitalfabricationlab/PostAdapter.java
+++ client/app/src/main/java/com/nilswesthoff/nils/digitalfabricationlab/PostAdapter.java
@@ -29,6 +29,8 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
     public Context mContext;
     public List<Post> mPost;
 
+    private FirebaseUser currentUser;
+
 
     public PostAdapter(Context mContext, List<Post> mPost) {
         this.mContext = mContext;
@@ -44,11 +46,12 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i){
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i){
 
-        //TODO: currentUser=FirebaseAuth.getInstance().getCurrentUser()
-        Post post=mPost.get(i);
-        Glide.with(mContext).load(post.getPostimage()).into(viewHolder:post_image);
+        currentUser=FirebaseAuth.getInstance().getCurrentUser();
+        final Post post=mPost.get(i);
+
+        Glide.with(mContext).load(post.getPostimage()).into(viewHolder.post_image);
 
         if (post.getDescription().equals("")){
             viewHolder.description.setVisibility(View.GONE);
@@ -57,6 +60,22 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
             viewHolder.description.setText(post.getDescription());
         }
         publisherInfo(viewHolder.image_profile, viewHolder.fullname,viewHolder.publisher, post.getPublisher());
+        isLiked(post.getPostid(), viewHolder.like);
+        nrLikes(viewHolder.likes, post.getPostid());
+
+        viewHolder.like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(viewHolder.like.getTag().equals("like")){
+                    FirebaseDatabase.getInstance().getReference().child("Likes").child(post.getPostid())
+                            .child(currentUser.getUid()).setValue(true);
+                } else {
+                    FirebaseDatabase.getInstance().getReference().child("Likes").child(post.getPostid())
+                            .child(currentUser.getUid()).removeValue();
+                }
+            }
+        });
+
 
     }
 
@@ -86,6 +105,50 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
         }
     }
 
+    private void isLiked (String postid, final ImageView imageView ){
+
+        final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference reference=FirebaseDatabase.getInstance().getReference()
+                .child("likes")
+                .child(postid);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(currentUser.getUid()).exists()){
+                    imageView.setImageResource(R.drawable.ic_liked);
+                    imageView.setTag("liked");
+                } else {
+                    imageView.setImageResource(R.drawable.ic_like);
+                    imageView.setTag("Like");
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void nrLikes(final TextView likes, String postid){
+        DatabaseReference reference=FirebaseDatabase.getInstance().getReference().child("Likes")
+                .child(postid);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                likes.setText(dataSnapshot.getChildrenCount()+"Likes");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     private void publisherInfo(ImageView image_profile, final TextView fullname, final TextView publisher, String userid) {
         DatabaseReference reference= FirebaseDatabase.getInstance().getReference("Users").child(userid);
 
@@ -93,7 +156,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 User user=dataSnapshot.getValue(User.class);
-                //TODO Glide.with(mContext).load.(Profile.getImageurl());
+                Glide.with(mContext).load(user.getImageurl());
                 //TODO wij hebben geen User met volgers, heb nu de tutorial gevolgd maar moeten dit nog even uitzoeken hoe we alles wat geupload wordt laten zien.
                 //https://www.youtube.com/watch?v=mk2CrU-awkM&list=PLzLFqCABnRQduspfbu2empaaY9BoIGLDM&index=7
                 fullname.setText(user.getFullname());
